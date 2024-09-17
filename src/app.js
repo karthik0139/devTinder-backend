c10a8760888e16e2bdf76fbc70066af42574d196
karthikamarneni <karthikamarneni110223@gmail.com>
2024-09-17 14:11:08 +0530

    intial commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res) => {
+    res.send("server is running ")
+})
+
+app.listen(8080,() => {
+     console.log("server started")
+})
