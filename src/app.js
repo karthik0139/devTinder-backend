9a92605a6409e0a851c40c3219e5f58d34d78c78
karthikamarneni <karthikamarneni110223@gmail.com>
2024-09-17 18:48:19 +0530

    api

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res) => {
-    res.send("server is running ")
+app.get('/user' , (req,res) => {
+    res.send({firstName:'karthik' , lastName:'Amarneni'})
 })
+
+app.post('/user', (req,res) => {
+     res.send("userData has been posted sucessfully")
+})
+
+app.delete('/user', (req,res) => {
+    res.send("userData is deleted sucessfully")
+})
+
 
 app.listen(8080,() => {
      console.log("server started")
