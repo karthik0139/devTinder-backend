0721ee3e92ad8cf2389e69b890518d25ee3f3203
karthikamarneni <karthikamarneni110223@gmail.com>
2024-09-22 09:26:18 +0530

    middelware

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
-const express = require('express');
+const express = require('express')
 const app = express();
 
-app.get('/user' , (req,res) => {
-    res.send({firstName:'karthik' , lastName:'Amarneni'})
-})
-
-app.post('/user', (req,res) => {
-     res.send("userData has been posted sucessfully")
-})
-
-app.delete('/user', (req,res) => {
-    res.send("userData is deleted sucessfully")
-})
+app.get('/user',
+   [ (req,res, next) => {
+        console.log("first server is called");
+        // res.send("first server return a data")
+        next();
+    },
+    (req,res,next) => {
+        console.log("second server is called")
+         res.send("second server return a data")
+        next()
+    }],
+    (req,res,next) => {
+        console.log("third server is called")
+        // res.send("third server returns data")
+        // next()
+    }
+)
 
 
 app.listen(8080,() => {
